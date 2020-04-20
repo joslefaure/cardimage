f636a7ffd444919bd52a165435cd8f9db50b0167
103071073 <josmyfaure@gmail.com>
2020-04-20 16:54:42 +0800

    Updates

---
+++ precache-manifest.f70880a67e6291b3fa7fefba633716b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72f8e879fb95a0dc47318a422a6d2410",
+    "url": "/cardimage/index.html"
+  },
+  {
+    "revision": "b19c25a5e07282302d0c",
+    "url": "/cardimage/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4a7132d36b7ef734f2c1",
+    "url": "/cardimage/static/js/2.d3fc0652.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cardimage/static/js/2.d3fc0652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b19c25a5e07282302d0c",
+    "url": "/cardimage/static/js/main.6ebd90c3.chunk.js"
+  },
+  {
+    "revision": "e06a06aeae3bd9c22502",
+    "url": "/cardimage/static/js/runtime-main.f17aaa2d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cardimage/static/media/logo.5d5d9eef.svg"
+  }
+]);
